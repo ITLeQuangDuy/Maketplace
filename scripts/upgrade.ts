083debbbc5c712c221de03be40522b4db7fccfe6
ITLeQuangDuy <it.lequangduy@gmail.com>
2024-01-11 16:05:05 +0700

    update testcase

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -4,25 +4,22 @@ import { getAllAddresses } from "./address/handleAddress";
 
 let [deployer, admin, user1, user2]: Signer[] = [];
 
-
-const upgradePokeball = async () => {
-  const { pokeballAddress } = getAllAddresses(
+const upgradeMaketplace = async () => {
+  const { addressMaketplace } = getAllAddresses(
     network.config.chainId?.toString()
   );
 
   [deployer] = await ethers.getSigners();
 
-  const pokeballFac = await ethers.getContractFactory("Pokeball", deployer);
+  const maketplaceFac = await ethers.getContractFactory("Maketplace", deployer);
 
-  await upgrades.upgradeProxy(pokeballAddress, pokeballFac);
+  await upgrades.upgradeProxy(addressMaketplace, maketplaceFac);
 };
 
 async function main() {
-    await upgradePokeball();
+    await upgradeMaketplace();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
